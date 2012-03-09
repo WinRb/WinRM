4a4eb91a4c152c3e2bf08ac66991b15e8160cb00
Dan Wanek <dan.wanek@gmail.com>
2012-03-09 08:41:16 -0600

    Move license from GPL to Apache 2.0

--- lib/winrm/winrm_service.rb
+++ lib/winrm/winrm_service.rb
@@ -1,3 +1,21 @@
+=begin
+  This file is part of WinRM; the Ruby library for Microsoft WinRM.
+
+  Copyright © 2010 Dan Wanek <dan.wanek@gmail.com>
+
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+=end
+
 module WinRM
   # This is the main class that does the SOAP request/response logic. There are a few helper classes, but pretty
   #   much everything comes through here first.
@@ -398,4 +416,3 @@ module WinRM
 
   end # WinRMWebService
 end # WinRM
-
