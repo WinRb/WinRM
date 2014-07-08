a13523b0afd8c66a12bbb25c8e0656e70540784a
Shawn Neal <sneal@sneal.net>
2014-07-08 13:14:35 -0700

    Get RSpec tests working in TravisCI
    - Ensure all gems in the Gemfile are available on the load path
    - Moved specs to more traditional directory structure
    - Require nori using correct case
    - Provide sane defaults for winrm connection info for unit tests

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,17 +1,25 @@
-$: << File.dirname(__FILE__) + '/../../lib/'
+require 'rubygems'
+require 'bundler/setup'
 require 'winrm'
 require 'json'
 
 module ConnectionHelper
-  # To run this test put a file called 'creds.json' in this directory with the following format:
-  #   {"user":"myuser","pass":"mypass","endpoint":"http://mysys.com/wsman","realm":"MY.REALM"}
-  WINRM_CONFIG = File.expand_path("#{File.dirname(__FILE__)}/../config.yml")
 
   def winrm_connection
-    config = symbolize_keys(YAML.load(File.read(WINRM_CONFIG)))
+    config = symbolize_keys(YAML.load(File.read(winrm_config_path)))
     config[:options].merge!( :basic_auth_only => true ) unless config[:auth_type].eql? :kerberos
     winrm = WinRM::WinRMWebService.new(config[:endpoint], config[:auth_type].to_sym, config[:options])
     winrm
+  end
+
+  def winrm_config_path
+    # Copy config-example.yml to config.yml and edit for your local configuration
+    path = File.expand_path("#{File.dirname(__FILE__)}/config.yml")
+    if !File.exists?(path)
+      # user hasn't done this, so use sane defaults for unit tests
+      path = File.expand_path("#{File.dirname(__FILE__)}/config-example.yml")
+    end
+    path
   end
 
   def symbolize_keys(hash)
