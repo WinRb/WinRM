4c851ff8cfe3e6721e9ac2b0fb4252f8ae89f604
Shawn Neal <sneal@sneal.net>
2014-07-31 08:28:26 -0700

    Cmd spec uses new output matchers

--- spec/cmd_spec.rb
+++ spec/cmd_spec.rb
@@ -1,42 +1,58 @@
-describe "Test remote WQL features via WinRM", :integration => true do
+describe 'winrm client cmd', :integration => true do
   before(:all) do
     @winrm = winrm_connection
   end
 
-  it 'should run a CMD command string' do
-    output = @winrm.run_cmd('ipconfig /all')
-    expect(output[:exitcode]).to eq(0)
-    expect(output[:data]).to_not be_empty
+  describe 'empty string' do
+    subject(:output) { @winrm.cmd('') }
+    it { should have_exit_code 0 }
+    it { should have_no_stdout }
+    it { should have_no_stderr }
   end
 
-  it 'should run a CMD command with proper arguments' do
-    output = @winrm.run_cmd('ipconfig', %w{/all})
-    expect(output[:exitcode]).to eq(0)
-    expect(output[:data]).to_not be_empty
+  describe 'ipconfig' do
+    subject(:output) { @winrm.cmd('ipconfig') }
+    it { should have_exit_code 0 }
+    it { should have_stdout_match /Windows IP Configuration/ }
+    it { should have_no_stderr }
   end
 
-  it 'should run a CMD command with block' do
-    outvar = ''
-    @winrm.run_cmd('ipconfig', %w{/all}) do |stdout, stderr|
-      outvar << stdout
+  describe 'echo \'hello world\' using apostrophes' do
+    subject(:output) { @winrm.cmd("echo 'hello world'") }
+    it { should have_exit_code 0 }
+    it { should have_stdout_match /'hello world'/ }
+    it { should have_no_stderr }
+  end
+
+  describe 'ipconfig with /all argument' do
+    subject(:output) { @winrm.cmd('ipconfig', %w{/all}) }
+    it { should have_exit_code 0 }
+    it { should have_stdout_match /Windows IP Configuration/ }
+    it { should have_no_stderr }
+  end
+
+  describe 'dir with incorrect argument /z' do
+    subject(:output) { @winrm.cmd('dir /z') }
+    it { should have_exit_code 1 }
+    it { should have_no_stdout }
+    it { should have_stderr_match /Invalid switch/ }
+  end
+
+  describe 'ipconfig && echo error 1>&2' do
+    subject(:output) { @winrm.cmd('ipconfig && echo error 1>&2') }
+    it { should have_exit_code 0 }
+    it { should have_stdout_match /Windows IP Configuration/ }
+    it { should have_stderr_match /error/ }
+  end
+
+  describe 'ipconfig with a block' do
+    subject(:stdout) do
+      outvar = ''
+      @winrm.cmd('ipconfig') do |stdout, stderr|
+        outvar << stdout
+      end
+      outvar
     end
-    expect(outvar).to match(/Windows IP Configuration/)
+    it { should match(/Windows IP Configuration/) }
   end
-
-  it 'should run a CMD command that contains an apostrophe' do
-    output = @winrm.run_cmd(%q{echo 'hello world'})
-    expect(output[:exitcode]).to eq(0)
-    expect(output[:data][0][:stdout]).to match(/'hello world'/)
-  end
-
-  it 'should run a CMD command that is empty' do
-    output = @winrm.run_cmd('')
-    expect(output[:exitcode]).to eq(0)
-  end
-
-  it 'should run a CMD command that has bad arg and return non-zero exit code' do
-    output = @winrm.run_cmd('dir /z')
-    expect(output[:exitcode]).to eq(1)
-  end
-
 end
