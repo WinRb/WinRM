ec4dc10f7505e45c53fb8cb047a2000ac752abb3
Shawn Neal <sneal@sneal.net>
2014-11-15 08:31:38 -0800

    Added working upload test

--- spec/file_manager_spec.rb
+++ spec/file_manager_spec.rb
@@ -1,13 +1,20 @@
 describe WinRM::FileManager, :integration => true do
   let(:service) { winrm_connection }
   let(:src_file) { __FILE__ }
-  #let(:dest_file) { File.join(dest_temp_dir, 'winrm_filemanager_test') }
+  let(:dest_file) { File.join(subject.temp_dir, 'winrm_filemanager_test') }
 
   subject { WinRM::FileManager.new(service) }
 
   context 'temp_dir' do
     it 'should return the remote guests temp dir' do
       expect(subject.temp_dir).to eq('C:\Users\vagrant\AppData\Local\Temp')
+    end
+  end
+
+  context 'upload' do
+    it 'should upload the specified file' do
+      subject.upload(src_file, dest_file)
+      expect(subject.exists?(dest_file)).to be true
     end
   end
 end
