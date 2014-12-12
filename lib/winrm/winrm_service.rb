18db7086d9851400d3aa46b1e4338d18a068dc9e
Max Lincoln <max@devopsy.com>
2014-12-11 23:48:17 -0500

    Note about multiline scripts

--- lib/winrm/winrm_service.rb
+++ lib/winrm/winrm_service.rb
@@ -151,12 +151,13 @@ module WinRM
 
       # Grab the command element and unescape any single quotes - issue 69
       xml = builder.target!
+      # Note: This won't match is the command spans multiple lines, should it have a multline flag (/m)?
       escaped_cmd = /<#{NS_WIN_SHELL}:Command>(.+)<\/#{NS_WIN_SHELL}:Command>/.match(xml)[1]
       xml.sub!(escaped_cmd, escaped_cmd.gsub(/&#39;/, "'"))
 
       resp_doc = send_message(xml)
       command_id = REXML::XPath.first(resp_doc, "//#{NS_WIN_SHELL}:CommandId").text
-      
+
       if block_given?
         begin
           yield command_id
