4c0d79f5bde9067636ad11c406f98b91452902e4
Shawn Neal <sneal@sneal.net>
2014-12-18 08:55:55 -0800

    Extracted md5 temp file resolver class

--- lib/winrm/file_transfer/remote_file.rb
+++ lib/winrm/file_transfer/remote_file.rb
@@ -2,6 +2,7 @@ require 'io/console'
 require 'json'
 require_relative 'command_executor'
 require_relative 'base64_temp_file_decoder'
+require_relative 'md5_temp_file_resolver'
 
 module WinRM
   class RemoteFile
@@ -9,7 +10,6 @@ module WinRM
     attr_reader :local_path
     attr_reader :remote_path
     attr_reader :temp_path
-    attr_reader :shell
 
     def initialize(command_executor, local_path, remote_path)
       @logger = Logging.logger[self]
@@ -22,18 +22,18 @@ module WinRM
       @logger.debug("Uploading file: #{@local_path} -> #{@remote_path}")
       raise WinRMUploadError.new("Cannot find path: #{@local_path}") unless File.exist?(@local_path)
 
-      @temp_path = @command_executor.run_powershell(resolve_tempfile_command).chomp
+      temp_file_resolver = WinRM::Md5TempFileResolver.new(@command_executor)
+      @temp_path = temp_file_resolver.temp_file_path(@local_path, @remote_path)
 
-      if !@temp_path.to_s.empty?
-        size = upload_to_tempfile(&block)
-        decoder = WinRM::Base64TempFileDecoder.new(@command_executor)
-        decoder.decode(@temp_path, @remote_path)
-      else
-        size = 0
+      if @temp_path.empty?
         @logger.debug("Files are equal. Not copying #{@local_path} to #{@remote_path}")
+        return 0
       end
 
-      return size
+      size = upload_to_tempfile(&block)
+      decoder = WinRM::Base64TempFileDecoder.new(@command_executor)
+      decoder.decode(@temp_path, @remote_path)
+      size
     rescue WinRMUploadError => e
       # add additional context, from and to
       raise WinRMUploadError,
@@ -59,33 +59,6 @@ module WinRM
         end
       end
       base64_array.length
-    end
-
-    def resolve_tempfile_command()
-      local_md5 = Digest::MD5.file(@local_path).hexdigest
-      <<-EOH
-        # get the resolved target path
-        $destFile = $ExecutionContext.SessionState.Path.GetUnresolvedProviderPathFromPSPath("#{@remote_path}")
-
-        # check if file is up to date
-        if (Test-Path $destFile) {
-          $cryptoProv = New-Object -TypeName System.Security.Cryptography.MD5CryptoServiceProvider
-
-          $file = [System.IO.File]::Open($destFile,
-            [System.IO.Filemode]::Open, [System.IO.FileAccess]::Read)
-          $guestMd5 = ([System.BitConverter]::ToString($cryptoProv.ComputeHash($file)))
-          $guestMd5 = $guestMd5.Replace("-","").ToLower()
-          $file.Close()
-
-          # file content is up to date, send back an empty file path to signal this
-          if ($guestMd5 -eq '#{local_md5}') {
-            return ''
-          }
-        }
-
-        # file doesn't exist or out of date, return a unique temp file path to upload to
-        return [System.IO.Path]::GetTempFileName()
-      EOH
     end
 
     def create_empty_destfile_command()
