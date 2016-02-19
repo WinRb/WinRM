c19de91a12bb7504d4c16317df7c47385f918bc5
Shawn Neal <sneal@sneal.net>
2016-02-19 10:15:56 -0800

    Add WSMV module with create shell message

---
+++ spec/create_shell_message_spec.rb
@@ -0,0 +1,40 @@
+# encoding: UTF-8
+
+require_relative '../lib/winrm/wsmv/create_shell_message'
+
+describe 'CreateShell', unit: true do
+  context 'default session options' do
+    session_opts = {
+      endpoint: 'http://localhost:5985/wsman',
+      max_envelope_size: 153600,
+      session_id: '05A2622B-B842-4EB8-8A78-0225C8A993DF',
+      timeout: 60,
+      locale: 'en-US'
+    }
+    it 'creates a well formed message' do
+      xml = WinRM::WSMV::CreateShellMessage.new(session_opts).build
+      expect(xml).to include('<w:OperationTimeout>PT60S</w:OperationTimeout>')
+      expect(xml).to include('<w:Locale xml:lang="en-US" mustUnderstand="false"/>')
+      expect(xml).to include('<p:DataLocale xml:lang="en-US" mustUnderstand="false"/>')
+      expect(xml).to include(
+        '<p:SessionId mustUnderstand="false">' \
+        'uuid:05A2622B-B842-4EB8-8A78-0225C8A993DF</p:SessionId>')
+      expect(xml).to include('<w:MaxEnvelopeSize mustUnderstand="true">153600</w:MaxEnvelopeSize>')
+      expect(xml).to include('<a:To>http://localhost:5985/wsman</a:To>')
+      expect(xml).to include('<rsp:InputStreams>stdin</rsp:InputStreams>')
+      expect(xml).to include('<rsp:OutputStreams>stdout stderr</rsp:OutputStreams>')
+      expect(xml).to include(
+        '<w:ResourceURI mustUnderstand="true">' \
+        'http://schemas.microsoft.com/wbem/wsman/1/windows/shell/cmd</w:ResourceURI>')
+    end
+    context 'shell options' do
+      let(:shell_opts) { Hash.new }
+      it 'includes environemt vars' do
+        shell_opts[:env_vars] = { 'FOO' => 'BAR' }
+        xml = WinRM::WSMV::CreateShellMessage.new(session_opts, shell_opts).build
+        expect(xml).to include(
+          '<rsp:Environment><rsp:Variable Name="FOO">BAR</rsp:Variable></rsp:Environment>')
+      end
+    end
+  end
+end
