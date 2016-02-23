be7f350120da45bc9645aaf7ee2b839c6391a92e
Shawn Neal <sneal@sneal.net>
2016-02-23 14:37:20 -0800

    Move create_pipeline spec to new location

--- tests/spec/wsmv/create_pipeline_spec.rb
+++ tests/spec/wsmv/create_pipeline_spec.rb
@@ -1,18 +1,9 @@
 # encoding: UTF-8
 
-require_relative '../lib/winrm/wsmv/create_pipeline'
+require 'winrm/wsmv/create_pipeline'
 
-describe 'CreatePipeline', unit: true do
+describe WinRM::WSMV::CreatePipeline do
   context 'default session options' do
-    let(:session_opts) do
-      {
-        endpoint: 'http://localhost:5985/wsman',
-        max_envelope_size: 153600,
-        session_id: '05A2622B-B842-4EB8-8A78-0225C8A993DF',
-        operation_timeout: 60,
-        locale: 'en-US'
-      }
-    end
     let(:shell_id) { 'D5A2622B-B842-4EB8-8A78-0225C8A993DF' }
     let(:command) { 'ipconfig' }
     let(:pipeline) do
@@ -20,11 +11,9 @@ describe 'CreatePipeline', unit: true do
         3, shell_id, subject.command_id, command)
       Base64.strict_encode64(message.bytes.pack('C*'))
     end
-
-    subject { WinRM::WSMV::CreatePipeline.new(session_opts, shell_id, command) }
-
+    subject { WinRM::WSMV::CreatePipeline.new(default_session_opts, shell_id, command) }
+    let(:xml) { subject.build }
     it 'creates a well formed message' do
-      xml = subject.build
       expect(xml).to include('<w:OperationTimeout>PT60S</w:OperationTimeout>')
       expect(xml).to include(
         '<w:SelectorSet><w:Selector Name="ShellId">' \
