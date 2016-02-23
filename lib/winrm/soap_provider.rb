019f140c15ef40b230329214cd297d929de4f741
Shawn Neal <sneal@sneal.net>
2016-02-22 17:55:28 -0800

    Remove soap namespace constants from soap provider

--- lib/winrm/soap_provider.rb
+++ lib/winrm/soap_provider.rb
@@ -19,21 +19,6 @@ require 'builder'
 require 'gyoku'
 require 'base64'
 
-# SOAP constants for WinRM
-module WinRM
-  NS_SOAP_ENV    = 's'   # http://www.w3.org/2003/05/soap-envelope
-  NS_ADDRESSING  = 'a'   # http://schemas.xmlsoap.org/ws/2004/08/addressing
-  NS_CIMBINDING  = 'b'   # http://schemas.dmtf.org/wbem/wsman/1/cimbinding.xsd
-  NS_ENUM        = 'n'   # http://schemas.xmlsoap.org/ws/2004/09/enumeration
-  NS_TRANSFER    = 'x'   # http://schemas.xmlsoap.org/ws/2004/09/transfer
-  NS_WSMAN_DMTF  = 'w'   # http://schemas.dmtf.org/wbem/wsman/1/wsman.xsd
-  NS_WSMAN_MSFT  = 'p'   # http://schemas.microsoft.com/wbem/wsman/1/wsman.xsd
-  NS_SCHEMA_INST = 'xsi' # http://www.w3.org/2001/XMLSchema-instance
-  NS_WIN_SHELL   = 'rsp' # http://schemas.microsoft.com/wbem/wsman/1/windows/shell
-  NS_WSMAN_FAULT = 'f'   # http://schemas.microsoft.com/wbem/wsman/1/wsmanfault
-  NS_WSMAN_CONF  = 'cfg' # http://schemas.microsoft.com/wbem/wsman/1/config
-end
-
 require 'winrm/exceptions/exceptions'
 require 'winrm/winrm_service'
 require 'winrm/http/transport'
