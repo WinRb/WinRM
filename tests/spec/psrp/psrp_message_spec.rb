bc6ca47c0b636d909f4fc5facab8a8f4885db097
Matt Wrock <matt@mattwrock.com>
2016-03-14 23:46:41 -0700

    add unit tests around message parsing

--- tests/spec/psrp/psrp_message_spec.rb
+++ tests/spec/psrp/psrp_message_spec.rb
@@ -153,7 +153,7 @@ describe WinRM::PSRP::Message do
   end
   context 'parse from bytes' do
     let(:bytes) do
-      "\x00\x00\x00\x00\x00\x00\x00\x04\x00\x00\x00\x00\x00\x00\x00\x00\x03\x00\x00\x00I\x01"\
+      "\x00\x00\x00\x00\x00\x00\x00\x04\x00\x00\x00\x00\x00\x00\x00\x01\x03\x00\x00\x00I\x01"\
       "\x00\x00\x00\x04\x10\x04\x00Kk/=Z\xD3-E\x81v\xA0+6\xB1\xD3\x88\n\xED\x90\x9Cj\xE7PG"\
       "\x9F\xA2\xB2\xC99to9\xEF\xBB\xBF<S>some data_x000D__x000A_</S>".to_byte_string
     end
@@ -161,6 +161,30 @@ describe WinRM::PSRP::Message do
 
     it 'parses the data' do
       expect(subject.data).to eq("\xEF\xBB\xBF<S>some data_x000D__x000A_</S>".to_byte_string)
+    end
+
+    it 'parses the destination' do
+      expect(subject.destination).to eq(1)
+    end
+
+    it 'parses the end_fragment' do
+      expect(subject.end_fragment).to eq(true)
+    end
+
+    it 'parses the start_fragment' do
+      expect(subject.start_fragment).to eq(true)
+    end
+
+    it 'parses the fragment id' do
+      expect(subject.fragment_id).to eq(1)
+    end
+
+    it 'parses the object id' do
+      expect(subject.object_id).to eq(4)
+    end
+
+    it 'parses the message type' do
+      expect(subject.message_type).to eq(WinRM::PSRP::Message::MESSAGE_TYPES[:pipeline_output])
     end
   end
 end
