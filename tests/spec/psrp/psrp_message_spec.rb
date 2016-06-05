1bfd200ec1761f1cb66fbc09558a53480c03bb3f
Matt Wrock <matt@mattwrock.com>
2016-06-04 22:14:27 -0700

    break up the message spec to specs of new classes

--- tests/spec/psrp/psrp_message_spec.rb
+++ tests/spec/psrp/psrp_message_spec.rb
@@ -4,102 +4,37 @@ require 'winrm/psrp/message'
 
 describe WinRM::PSRP::Message do
   context 'all fields provided' do
-    subject(:bytes) do
-      payload = <<-HERE.unindent
-        <Obj RefId="0"><MS><I32 N="MinRunspaces">1</I32><I32 N="MaxRunspaces">1</I32>
-        <Obj N="PSThreadOptions" RefId="1"><TN RefId="0">
-        <T>System.Management.Automation.Runspaces.PSThreadOptions</T><T>System.Enum</T>
-        <T>System.ValueType</T><T>System.Object</T></TN><ToString>Default</ToString><I32>0</I32>
-        </Obj><Obj N="ApartmentState" RefId="2"><TN RefId="1"><T>System.Threading.ApartmentState</T>
-        <T>System.Enum</T><T>System.ValueType</T><T>System.Object</T></TN>
-        <ToString>Unknown</ToString><I32>2</I32></Obj><Obj N="ApplicationArguments" RefId="3">
-        <TN RefId="2"><T>System.Management.Automation.PSPrimitiveDictionary</T>
-        <T>System.Collections.Hashtable</T><T>System.Object</T></TN><DCT><En>
-        <S N="Key">PSVersionTable</S><Obj N="Value" RefId="4"><TNRef RefId="2" /><DCT><En>
-        <S N="Key">PSVersion</S><Version N="Value">5.0.11082.1000</Version></En><En>
-        <S N="Key">PSCompatibleVersions</S><Obj N="Value" RefId="5"><TN RefId="3">
-        <T>System.Version[]</T><T>System.Array</T><T>System.Object</T></TN><LST>
-        <Version>1.0</Version><Version>2.0</Version><Version>3.0</Version><Version>4.0</Version>
-        <Version>5.0.11082.1000</Version></LST></Obj></En><En><S N="Key">CLRVersion</S>
-        <Version N="Value">4.0.30319.42000</Version></En><En><S N="Key">BuildVersion</S>
-        <Version N="Value">10.0.11082.1000</Version></En><En><S N="Key">WSManStackVersion</S>
-        <Version N="Value">3.0</Version></En><En><S N="Key">PSRemotingProtocolVersion</S>
-        <Version N="Value">2.3</Version></En><En><S N="Key">SerializationVersion</S>
-        <Version N="Value">1.1.0.1</Version></En></DCT></Obj></En></DCT></Obj>
-        <Obj N="HostInfo" RefId="6"><MS><Obj N="_hostDefaultData" RefId="7"><MS>
-        <Obj N="data" RefId="8"><TN RefId="4"><T>System.Collections.Hashtable</T>
-        <T>System.Object</T></TN><DCT><En><I32 N="Key">9</I32><Obj N="Value" RefId="9"><MS>
-        <S N="T">System.String</S><S N="V">C:\dev\kitchen-vagrant</S></MS></Obj></En><En>
-        <I32 N="Key">8</I32><Obj N="Value" RefId="10"><MS>
-        <S N="T">System.Management.Automation.Host.Size</S><Obj N="V" RefId="11"><MS>
-        <I32 N="width">199</I32><I32 N="height">52</I32></MS></Obj></MS></Obj></En><En>
-        <I32 N="Key">7</I32><Obj N="Value" RefId="12"><MS>
-        <S N="T">System.Management.Automation.Host.Size</S><Obj N="V" RefId="13"><MS>
-        <I32 N="width">80</I32><I32 N="height">52</I32></MS></Obj></MS></Obj></En><En>
-        <I32 N="Key">6</I32><Obj N="Value" RefId="14"><MS>
-        <S N="T">System.Management.Automation.Host.Size</S><Obj N="V" RefId="15"><MS>
-        <I32 N="width">80</I32><I32 N="height">25</I32></MS></Obj></MS></Obj></En><En>
-        <I32 N="Key">5</I32><Obj N="Value" RefId="16"><MS>
-        <S N="T">System.Management.Automation.Host.Size</S><Obj N="V" RefId="17"><MS>
-        <I32 N="width">80</I32><I32 N="height">9999</I32></MS></Obj></MS></Obj></En><En>
-        <I32 N="Key">4</I32><Obj N="Value" RefId="18"><MS><S N="T">System.Int32</S>
-        <I32 N="V">25</I32></MS></Obj></En><En><I32 N="Key">3</I32><Obj N="Value" RefId="19"><MS>
-        <S N="T">System.Management.Automation.Host.Coordinates</S><Obj N="V" RefId="20"><MS>
-        <I32 N="x">0</I32><I32 N="y">9974</I32></MS></Obj></MS></Obj></En><En><I32 N="Key">2</I32>
-        <Obj N="Value" RefId="21"><MS><S N="T">System.Management.Automation.Host.Coordinates</S>
-        <Obj N="V" RefId="22"><MS><I32 N="x">0</I32><I32 N="y">9998</I32></MS></Obj></MS></Obj></En>
-        <En><I32 N="Key">1</I32><Obj N="Value" RefId="23"><MS><S N="T">System.ConsoleColor</S>
-        <I32 N="V">0</I32></MS></Obj></En><En><I32 N="Key">0</I32><Obj N="Value" RefId="24"><MS>
-        <S N="T">System.ConsoleColor</S><I32 N="V">7</I32></MS></Obj></En></DCT></Obj></MS></Obj>
-        <B N="_isHostNull">false</B><B N="_isHostUINull">false</B><B N="_isHostRawUINull">false</B>
-        <B N="_useRunspaceHost">false</B></MS></Obj></MS></Obj>
-      HERE
-      msg = WinRM::PSRP::Message.new(
+    let(:payload) { 'this is my payload' }
+    subject do
+      described_class.new(
         'bc1bfbba-8215-4a04-b2df-7a3ac0310e16',
         WinRM::PSRP::Message::MESSAGE_TYPES[:session_capability],
         payload,
         '4218a578-0f18-4b19-82c3-46b433319126')
-      WinRM::PSRP::Fragment.new(1, msg.bytes).bytes
     end
-    it 'sets the message id to 1' do
-      expect(bytes[0..7]).to eq([0, 0, 0, 0, 0, 0, 0, 1])
-    end
-    it 'sets the fragment id to 0' do
-      expect(bytes[8..15]).to eq([0, 0, 0, 0, 0, 0, 0, 0])
-    end
-    it 'clears 6 reserved bits' do
-      expect(bytes[16] & 0b11111100).to eq(0)
-    end
-    it 'sets end fragment bit' do
-      expect(bytes[16] & 0b00000010).to eq(2)
-    end
-    it 'sets start fragment bit' do
-      expect(bytes[16] & 0b00000001).to eq(1)
-    end
-    it 'sets message blob length to 3640' do
-      expect(bytes[17..20]).to eq([0, 0, 14, 56])
-    end
+
     it 'sets the destination to server LE' do
-      expect(bytes[21..24]).to eq([2, 0, 0, 0])
+      expect(subject.bytes[0..3]).to eq([2, 0, 0, 0])
     end
     it 'sets the message type LE' do
-      expect(bytes[25..28]).to eq([2, 0, 1, 0])
+      expect(subject.bytes[4..7]).to eq([2, 0, 1, 0])
     end
     it 'sets the runspace pool id' do
-      expect(bytes[29..44]).to eq(
+      expect(subject.bytes[8..23]).to eq(
         [186, 251, 27, 188, 21, 130, 4, 74, 178, 223, 122, 58, 192, 49, 14, 22])
     end
     it 'sets the pipeline id' do
-      expect(bytes[45..60]).to eq(
+      expect(subject.bytes[24..39]).to eq(
         [120, 165, 24, 66, 24, 15, 25, 75, 130, 195, 70, 180, 51, 49, 145, 38])
     end
     it 'prefixes the blob with BOM' do
-      expect(bytes[61..63]).to eq([239, 187, 191])
+      expect(subject.bytes[40..42]).to eq([239, 187, 191])
     end
     it 'contains at least the first 8 bytes of the XML payload' do
-      expect(bytes[64..71]).to eq([60, 79, 98, 106, 32, 82, 101, 102])
+      expect(subject.bytes[43..-1]).to eq(payload.bytes)
     end
   end
+
   context 'create' do
     it 'raises error when message type is not valid' do
       expect do
@@ -111,6 +46,7 @@ describe WinRM::PSRP::Message do
       end.to raise_error(RuntimeError)
     end
   end
+
   context 'no command id' do
     subject(:msg) do
       payload = <<-HERE.unindent
@@ -123,59 +59,9 @@ describe WinRM::PSRP::Message do
         WinRM::PSRP::Message::MESSAGE_TYPES[:session_capability],
         payload)
     end
-    it 'does not error' do
-      expect { msg.bytes }.to_not raise_error
-    end
+
     it 'sets the pipeline id to empty' do
       expect(msg.bytes[24..39]).to eq([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
-    end
-  end
-  context 'parse from bytes' do
-    let(:bytes) do
-      "\x00\x00\x00\x00\x00\x00\x00\x04\x00\x00\x00\x00\x00\x00\x00\x01\x03\x00\x00\x00I\x01"\
-      "\x00\x00\x00\x04\x10\x04\x00Kk/=Z\xD3-E\x81v\xA0+6\xB1\xD3\x88\n\xED\x90\x9Cj\xE7PG"\
-      "\x9F\xA2\xB2\xC99to9\xEF\xBB\xBF<S>some data_x000D__x000A_</S>".to_byte_string
-    end
-    subject { WinRM::PSRP::MessageDefragmenter.new.defragment(Base64.encode64(bytes)) }
-
-    it 'parses the data' do
-      expect(subject.data).to eq("\xEF\xBB\xBF<S>some data_x000D__x000A_</S>".to_byte_string)
-    end
-
-    it 'parses the destination' do
-      expect(subject.destination).to eq(1)
-    end
-
-    it 'parses the message type' do
-      expect(subject.type).to eq(WinRM::PSRP::Message::MESSAGE_TYPES[:pipeline_output])
-    end
-
-    context 'parse fragment bytes' do
-      subject do
-        WinRM::PSRP::Fragment.new(
-          bytes[0..7].reverse.unpack('Q')[0],
-          bytes[21..-1],
-          bytes[8..15].reverse.unpack('Q')[0],
-          bytes[16].unpack('C')[0][0] == 1,
-          bytes[16].unpack('C')[0][1] == 1
-        )
-      end
-
-      it 'parses the end_fragment' do
-        expect(subject.end_fragment).to eq(true)
-      end
-
-      it 'parses the start_fragment' do
-        expect(subject.start_fragment).to eq(true)
-      end
-
-      it 'parses the fragment id' do
-        expect(subject.fragment_id).to eq(1)
-      end
-
-      it 'parses the object id' do
-        expect(subject.object_id).to eq(4)
-      end
     end
   end
 end
