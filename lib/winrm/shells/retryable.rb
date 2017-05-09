6f2ebf8946ed7bd1a8a3c0ed1575d79f21b0880e
Stallings, Ryan <Ryan.Stallings@ncr.com>
2017-05-09 12:35:26 +0000

    Updated retryable to add option list of faults to not retry.  Updated cleanup_command to pass expected errors to retryable so they won't be retried

--- lib/winrm/shells/retryable.rb
+++ lib/winrm/shells/retryable.rb
@@ -24,13 +24,15 @@ module WinRM
       # Retries the operation a specified number of times with a delay between
       # @param retries [Integer] The number of times to retry
       # @param delay [Integer] The number of seconds to wait between retry attempts
-      def retryable(retries, delay)
+      # @param no_retry [Array of fault codes] Errors that are expected or unrecoverable. Don't retry
+      def retryable(retries, delay, no_retry = [])
         yield
-      rescue *WinRM::NETWORK_EXCEPTIONS.call
-        raise unless (retries -= 1) > 0
+      rescue *WinRM::NETWORK_EXCEPTIONS.call => e
+        raise if no_retry.include?(e.fault_code) || (retries -= 1) <= 0
         sleep(delay)
         retry
       end
     end
   end
 end
+
